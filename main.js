b2ab2dbdfb94db7ce491da0de6e92f1def757a3d
Ch4s3d <ramozchris@gmail.com>
2022-08-03 16:45:50 -0500

    First Commit

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
-class Mundo {
+class Celula {
+    constructor(life, x, y) {
+        this.life = false;
+        this.x = x;
+        this.y = y;
+    }
+}
 
+class Mundo {
+    celula = [,];
+    
     constructor(columns, rows) {
         this.columns = columns;
         this.rows = rows;
     }
+
+    inicio() {
+        for (var i = 0; i < columns; i++){
+            for (var j = 0; j < rows; j++){
+                celula[i, j] = new Celula(false, i, j);
+            }
+        }
+    }
+
+    toString() {
+        var output = "";
+
+        for (var i = 0; i < columns; i++){
+            for (var j = 0; j < rows; j++){
+                if (celula[i, j].life == true)
+                    output += '*'
+                else
+                    output += '.'
+            }
+            output += '\n'
+        }
+        
+        return output;
+    }
 }
+function rules() {
+    
+}
+
+var columns = prompt("Numero de columnas");
+var rows = prompt("Numero de filas");
+var mundo = new Mundo(columns, rows);
+mundo.inicio();
+
+var n = prompt("Numero de celulas vivas");
+for (var i = 0; i < n; i++){
+    mundo.celula[Math.floor(Math.random() + columns), Math.floor(Math.random() + rows)].life = true;
+}
+
+console.log(mundo.toString())
